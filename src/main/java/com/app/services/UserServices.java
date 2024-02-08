cbb1e0c1c721945e7be9770e61c28a21d1f6b330
Alex <alex_alex3@inbox.lv>
2024-02-08 19:41:03 +0200

    Initial commit

---
+++ src/main/java/com/app/services/UserServices.java
@@ -0,0 +1,31 @@
+package com.app.services;
+
+import com.app.dao.UserDAO;
+import com.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServices {
+    @Autowired
+    private UserDAO userDAO;
+
+    public void storeNewUser(User user) {
+       //Field data validation here
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+
+        userDAO.storeNewUser(user);
+
+    }
+
+    public List<User> getAllUsers () {
+        return userDAO.getAllUsers();
+    }
+}
